75ebab8987ac6633aba946233d5103fc5ae7b806
MhmtDkrn <mehmetdikran47@gmail.com>
2023-10-01 07:15:17 +0300

    new commir

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Atama ve işlemli atamalar
+int x=5;
+int y=4;
+y+=3;
+Console.WriteLine(y);
+y/=1;
+Console.WriteLine(y);
+x-=3;
+Console.WriteLine(x);
+
+//Mantıksal Operatörler (&&, ||, !)
+bool isSuccess = true;
+bool isCompleted = false;
+
+if (isSuccess && isCompleted)
+    Console.WriteLine("Perfect!");
+if (isSuccess || isCompleted)
+    Console.WriteLine("Awesome!");
+if (isSuccess != isCompleted)
+    Console.WriteLine("Fine!");
